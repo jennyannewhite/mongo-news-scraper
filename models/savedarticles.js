92753a660bc8bc9f3a174ddb4bb9b5a6fe3b6d84
Jennifer <white.jenniferann@gmail.com>
2018-04-09 12:52:09 -0500

    second commit

---
+++ models/savedarticles.js
@@ -0,0 +1,46 @@
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create the Schema class
+var Schema = mongoose.Schema;
+
+// new Schema: UserSchema
+var savedSchema = new Schema({
+    // title: a trimmed, required string
+    title: {
+        type: String,
+        trim: true,
+        required: "Article title is required"
+    },
+    // link: a trimmed, required string
+    link: {
+        type: String,
+        unique: true,
+        required: "Link is Required"
+    },
+    // image: a string
+    image: {
+        type: String,
+    },
+    // text: a string
+    text: {
+        type: String,
+    },
+    // notes: associate the savedArticles model with the Notes model
+    notes: {
+        type: Schema.Types.ObjectId,
+        ref: 'Notes'
+    },
+    // articleCreated: the current date
+    articleCreated: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+// Use the above schema to make the savedData model/collection
+var Articles = mongoose.model("savedData", savedSchema);
+
+// Export the model so the server can use it
+module.exports = Articles;
---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+// Initialize Express
+var app = express();
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Load handlebars and designate main.handlebars as the default file
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//Imports the controller.js file
+var routes = require('./controllers/controller.js');
+app.use('/', routes);
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+});
